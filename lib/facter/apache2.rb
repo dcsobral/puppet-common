48687a6da9b4bf4f8c8a1983bcc967d02621999e
Daniel Sobral <dcsobral@gmail.com>
2011-05-05 21:05:07 -0300

    Make apache2 work on RedHat as well.

--- lib/facter/apache2.rb
+++ lib/facter/apache2.rb
@@ -2,7 +2,7 @@
 
 Facter.add("apache2") do
         setcode do
-                %x{/usr/bin/test -x /etc/init.d/apache2 && /bin/echo yes || /bin/echo no}.chomp
+                %x{/usr/bin/test -x /etc/init.d/apache2 -o -x /etc/rc.d/init.d/apache2 && /bin/echo yes || /bin/echo no}.chomp
         end
 end
 
